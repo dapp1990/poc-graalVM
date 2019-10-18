1f13dc9add9d7af4c29869450813d5be770c0fa6
Daniel Perez <dapp.1990@gmail.com>
2019-10-18 14:40:48 +0200

    Add workaround for jar issue

--- src/main/java/poc/graalvm/MainController.java
+++ src/main/java/poc/graalvm/MainController.java
@@ -5,13 +5,30 @@ import org.graalvm.polyglot.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+
 @RestController
 public class MainController {
 
     @GetMapping()
     public String index(){
+
+        // please see https://github.com/oracle/graal/issues/1348#issuecomment-534541812
+        try {
+            URL res = com.oracle.js.parser.ScriptEnvironment.class.getClassLoader().getResource("/META-INF/truffle/language");
+            // initialize the file system for the language file
+            if (res != null) {
+                FileSystems.newFileSystem(res.toURI(), new HashMap<>());
+            }
+        } catch (Throwable ignored) {
+            // in case of starting without fat jar
+        }
+
         Context context = Context.create();
         Value testing = context.eval("js", "(function () {return 'Executed by GraalJS'})()");
+
         return testing.toString();
     }
 }
